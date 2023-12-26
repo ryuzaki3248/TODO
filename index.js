a8b597fdbb67251ff65ac71c850720997c67ad5a
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-26 13:48:14 +0900

    ローカルストレージ使用

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos) {
+  todos.forEach(todo => {
+  add(todo);
+	})
+}
+
 form.addEventListener("submit",function(event){
 	event.preventDefault();
 	add();
 });
  
-function add(){
+function add(todo){
+  let todoText = input.value;
+
+  if (todo) {
+  todoText = todo;
+	}
+	
+	if (todoText.length > 0) {
   const li = document.createElement("li");
-	li.innerText = input.value;
+	li.innerText = todoText;
 	li.classList.add("list-group-item");
 	ul.appendChild(li);
 	input.value ="";
+	saveData();
+  }
+}
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+	let todos = [];
+	lists.forEach(list => {
+	todos.push(list.innerText);
+	})
+	localStorage.setItem("todos", JSON.stringify(todos));
 }
