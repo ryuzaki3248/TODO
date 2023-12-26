c76f2c5856005837b3f6b553440460b58fa2a46d
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-26 12:37:18 +0900

    フォームの入力値をファイルから取得

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// コメント
-console.log('Hello');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit",function(event){
+	event.preventDefault();
+  console.log(input.value);
+});
+ 
